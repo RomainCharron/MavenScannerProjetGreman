a4e0b1e4222fcbd17c1a9d378503ce886e4ce795
Formation <Formation@M2i-JAV5-04>
2018-03-20 10:04:24 +0100

    Veretion alpha 0.1

---
+++ src/main/java/com/mycompany/maxencejava/Equipe.java
@@ -0,0 +1,45 @@
+package com.mycompany.maxencejava;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Formation
+ */
+public class Equipe implements Ressource {
+    private String nom;
+    private String jeux;
+    private  List <Joueur> ListeJoueurs = new ArrayList <Joueur> ();
+     public Equipe(String nom, List  ListeJoueurs ) {
+        this.nom = nom;
+        this.ListeJoueurs = ListeJoueurs ; 
+    }
+    
+    public List <Joueur> getListeJoueurs() {
+        return ListeJoueurs;
+    }		
+    public String getNom() {
+        return nom;
+    }
+    @Override
+    public String getJeux() {
+        return jeux;
+    }
+    @Override
+    public void setJeux(String jeux) {
+        this.jeux = jeux;
+    }
+    @Override
+    public Color getCouleur() {
+        return Color.RED;
+    }
+    
+}
